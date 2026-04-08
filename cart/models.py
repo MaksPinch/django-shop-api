7fd52a149914a9b7615f4b4b6c0f365f41286991
Maksim Pinchuk <pinchukmaksim013@gmail.com>
2026-04-08 21:55:49 +0200

    created models for the catalog and cart apps

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from catalog.models import Product
+from django.contrib.auth.models import User
 # Create your models here.
+class CartItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='cart_items', verbose_name='Покупатель')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='cart_items', verbose_name='Товар')
+    quantity = models.PositiveIntegerField(default=1, verbose_name='Количество')
+
+def __str__(self):
+    return f'{self.user.username}: {self.product.name} = {self.quantity}'
+
+
--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Название')
+    slug = models.SlugField(max_length=200, unique=True, verbose_name='URL-адрес')
+    image = models.ImageField(upload_to='categories/', verbose_name='Изображение')
+
+    def __str__(self):
+        return self.name
+
+class Subcategory(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Название')
+    slug = models.SlugField(max_length=200, unique=True, verbose_name='URL-адрес')
+    image = models.ImageField(upload_to='subcategories/', verbose_name='Изображение')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='subcategories', verbose_name='Категория')
+
+    def __str__(self):
+        return f'{self.category.name}: {self.name}'
+
+class Product(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Название')
+    slug = models.SlugField(max_length=200, unique=True, verbose_name='URL-адрес')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
+    subcategory = models.ForeignKey(Subcategory, on_delete=models.CASCADE, related_name='products', verbose_name='Подкатегория')
+
+    def __str__(self):
+        return self.name
