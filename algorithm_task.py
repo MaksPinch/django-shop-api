743205c97536286a8c9ea34da8a445d2a7bef1b8
Maksim Pinchuk <pinchukmaksim013@gmail.com>
2026-04-14 16:35:57 +0200

    added algorithm_task.py and changed README.md

---
+++ algorithm_task.py
@@ -0,0 +1,20 @@
+# Напишите программу, которая выводит n первых элементов последовательности 122333444455555…
+# (число повторяется столько раз, чему оно равно).
+
+def func(x):
+    result = []
+    num = 1
+
+    while len(result) < x:
+
+        for _ in range(num):
+            if len(result) < x:
+                result.append(str(num))
+
+        num+=1
+
+    return "".join(result)
+
+print(func(7))
+print(func(5))
+print(func(10))
